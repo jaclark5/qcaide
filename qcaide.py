fa28dc7cec8b868cebf8ec16c47415ab522bc710
Brent Westbrook <brentrwestbrook@gmail.com>
2024-09-10 12:12:13 -0400

    initial code commit

---
+++ qcaide.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import sys
+
+import tomllib
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+
+from jinja2 import BaseLoader, Environment
+
+readme_tmpl = """
+# {{sub.name}}
+
+## Description
+{{sub.description -}}
+
+## General Information
+
+* Date: {{date}}
+* Class: {{sub.class_}}
+* Purpose: {{sub.purpose}}
+* Name: {{sub.name}}
+* Number of unique molecules: {{num_uniq}}
+* Number of filtered molecules: {{num_filt}}
+* Number of conformers: {{num_conf}}
+* Number of conformers per molecule (min, mean, max): {{conf_min}}, \
+{{conf_mean}}, {{conf_max}}
+* Mean molecular weight: {{mean_weight}}
+* Max molecular weight: {{max_weight}}
+* Charges: {{charges}}
+* Dataset submitter: {{sub.submitter}}
+* Dataset generator: {{sub.generator}}
+
+## QCSubmit Generation Pipeline
+
+{% for p in sub.pipeline %}
+{% if p["filename"] %}
+* `{{p["filename"]}}`: {{p["description"]}}
+{% else %}
+* {{p["description"]}}
+{% endif %}
+{% endfor %}
+
+## QCSubmit Manifest
+
+{% for p in sub.manifest %}
+* `{{p["filename"]}}`: {{p["description"]}}
+{% endfor %}
+
+## Metadata
+
+* elements: {{elements}}
+* Spec: {{spec}}
+"""
+
+readme_tmpl = Environment(loader=BaseLoader()).from_string(readme_tmpl)
+
+torsion_drive_line = (
+    "| `{{name}}` | [{{date_name}}]"
+    "(https://github.com/openforcefield/qca-dataset-submission/tree/master/"
+    "submissions/{{date_name}}) | {{short_description}} | | |"
+)
+torsion_drive_line = Environment(loader=BaseLoader()).from_string(
+    torsion_drive_line
+)
+
+
+class Submission:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_toml(cls, filename):
+        self = cls()
+        with open(filename, "rb") as f:
+            data = tomllib.load(f)
+        self.name = data["name"]
+        self.description = data["description"]
+        self.short_description = data["short_description"]
+        match data["class"]:
+            case "torsiondrive":
+                self.class_ = "OpenFF TorsionDrive Dataset"
+            case "optimization":
+                self.class_ = "OpenFF Optimization Dataset"
+            case misc:
+                print(
+                    f"warning: unrecognized dataset class: {misc}",
+                    file=sys.stderr,
+                )
+        self.purpose = data["purpose"]
+        self.submitter = data["submitter"]
+
+        if gen := data.get("generator", None):
+            self.generator = gen
+        else:
+            self.generator = self.submitter
+
+        self.pipeline = data["pipeline"]
+        self.manifest = data["manifest"]
+
+        return self
+
+
+@dataclass
+class Metadata:
+    num_uniq: int
+    num_filt: int
+    num_conf: int
+    conf_min: int
+    conf_mean: float
+    conf_max: int
+    mean_weight: float
+    max_weight: float
+    charges: set[int]
+    elements: set[str]
+    spec: dict[str, str]
+
+    @classmethod
+    def from_json(cls, filename):
+        with open(filename) as f:
+            data = json.load(f)
+        return cls(**data)
+
+
+def create(args):
+    sub = Submission.from_toml(args.input_file)
+    submissions = Path("submissions/")
+    date = datetime.today().strftime("%Y-%m-%d")
+    base = "-".join(sub.name.split())
+    date_name = f"{date}-{base}"
+    dir_ = submissions / date_name
+    print(f"creating {dir_}")
+    dir_.mkdir(exist_ok=True)
+    readme = Path(dir_ / "README.md")
+    if readme.exists():
+        exit(0)
+    with open(readme, "w") as out:
+        print(readme_tmpl.render(sub=sub, date=date), file=out)
+
+
+def readme(args):
+    sub = Submission.from_toml(args.input_file)
+    date = datetime.today().strftime("%Y-%m-%d")
+    base = "-".join(sub.name.split())
+    date_name = f"{date}-{base}"
+    print(
+        torsion_drive_line.render(
+            name=sub.name,
+            date_name=date_name,
+            short_description=sub.short_description,
+        )
+    )
+
+
+parser = argparse.ArgumentParser(prog="qcaide")
+subparsers = parser.add_subparsers(required=True, help="sub-command help")
+
+create_p = subparsers.add_parser("create")
+create_p.add_argument("input_file")
+create_p.set_defaults(func=create)
+
+readme_p = subparsers.add_parser("readme")
+readme_p.add_argument("input_file")
+readme_p.set_defaults(func=readme)
+
+args = parser.parse_args()
+args.func(args)
